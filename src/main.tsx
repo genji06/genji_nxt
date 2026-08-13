d4d806bd101e854bf135f42258be1cfc4b4e3a3c
hiroee-v <jeromevargas194@gmail.com>
2026-08-13 23:00:11 +0800

    fix icon

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import profile from "./assets/images/profile.png"
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import profile from "./assets/images/profile.png";
+import App from "./App.tsx";
 
-
-const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement
+const favicon = document.querySelector('link[rel="icon"]') as HTMLLinkElement;
 
 if (favicon) {
-  favicon.href = profile
+  favicon.href = profile;
 }
 
-createRoot(document.getElementById('root')!).render(
+const appleIcon = document.querySelector(
+  'link[rel="apple-touch-icon"]'
+) as HTMLLinkElement;
+
+if (appleIcon) {
+  appleIcon.href = profile;
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
